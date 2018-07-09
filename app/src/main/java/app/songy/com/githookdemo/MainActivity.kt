ba5784d26dc37873432e58407e988ae847bca4ca
songy <songy@aicaigroup.com>
2018-07-09 14:05:20 +0800

    #git hook pre commit msg modify test

--- app/src/main/java/app/songy/com/githookdemo/MainActivity.kt
+++ app/src/main/java/app/songy/com/githookdemo/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         tv_text.text="git hook"
         tv_text.setOnClickListener {
             Toast.makeText(this,"git hook",Toast.LENGTH_LONG).show()
